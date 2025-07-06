a77283e791f36a65b49dee636e1aca13c3a2da41
virtualpuneet <45757139+virtualpuneet@users.noreply.github.com>
2025-07-06 17:53:42 +0800

    final commit

--- Base2Dec.py
+++ Base2Dec.py
@@ -1,18 +1,12 @@
 # To convert number in your Base to your Base 10
 m = int(input ('your base:'))
 n = int(input ('your base number:'))
-#fin_out=str()
+fin_out=0
 #split the number as seperate digits and multiply base to the power 0, 1, 2 and ...
-
-while n//m != 0:
-    rem=n%m
-    #print (rem)
-    out=str(rem)
-    fin_out = out+fin_out
-    #print(fin_out)
-    n= n//m
-
-#print (str(n%m))
-out=str(n%m)
-fin_out = out+fin_out
-print("Number in Base", m, "is equal to: ", (int(fin_out)))
+str_num = str(n)
+str_len = len(str_num)
+for i in str_num:
+    str_len=str_len-1
+    out=int(int(i)*(m**str_len))
+    fin_out=fin_out+out
+print("Base 10 equivalent is: ", fin_out)
